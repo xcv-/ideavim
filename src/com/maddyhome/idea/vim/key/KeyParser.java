1a09080a74f3f3f4954710cd6fddd910e1b5dff8
rmaddy <rmaddy>
2005-10-29 21:28:03 +0000

    Disabling plugin now restores shortcuts to original order

--- src/com/maddyhome/idea/vim/key/KeyParser.java
+++ src/com/maddyhome/idea/vim/key/KeyParser.java
@@ -88,18 +88,28 @@ public class KeyParser
         return instance;
     }
 
+    /**
+     * This is called each time the plugin is enabled. This goes through the list of keystrokes that the plugin needs
+     * such as Ctrl-A through Ctrl-Z, F1, and others. Each keystroke is checks for existing IDEA actions using the
+     * keystroke and the keystroke is removed from the IDEA action. The keystroke is then added to the internal
+     * VimKeyHandler action.
+     */
     public void setupShortcuts()
     {
         Keymap keymap = KeymapManager.getInstance().getActiveKeymap();
+        // Loop through each of the keystrokes the plugin needs to take ownership of.
         Iterator keys = mappings.keySet().iterator();
         while (keys.hasNext())
         {
             KeyStroke keyStroke = (KeyStroke)keys.next();
+            // Get all the IDEA actions that use this keystroke. Could be 0 or more.
             ArrayList actions = (ArrayList)mappings.get(keyStroke);
             String[] ids = keymap.getActionIds(keyStroke);
+            // For each existing IDEA action we need to remove shortcut.
             for (int i = 0; i < ids.length; i++)
             {
                 String id = ids[i];
+                // Get the list of shortcuts for the IDEA action and find the one that matches the current keystroke
                 com.intellij.openapi.actionSystem.Shortcut[] cuts = keymap.getShortcuts(id);
                 for (int j = 0; j < cuts.length; j++)
                 {
@@ -109,7 +119,8 @@ public class KeyParser
                         if (((KeyboardShortcut)cut).getFirstKeyStroke().equals(keyStroke))
                         {
                             keymap.removeShortcut(id, cut);
-                            actions.add(id);
+                            // Save off the position of the shortcut so it can be put back in the same place.
+                            actions.add(new KeyAction(id, j));
                             keymap.addShortcut("VimKeyHandler", cut);
                         }
                     }
@@ -118,22 +129,48 @@ public class KeyParser
         }
     }
 
+    /**
+     * This is called each time the plugin is disabled. This processes all the keystrokes originally removed from their
+     * original IDEA action and are put back into place.
+     */
     public void resetShortcuts()
     {
         Keymap keymap = KeymapManager.getInstance().getActiveKeymap();
+        // Get each of the hijacked keystrokes we stole from IDEA and put them back in their original place.
         com.intellij.openapi.actionSystem.Shortcut[] cuts = keymap.getShortcuts("VimKeyHandler");
         for (int i = 0; i < cuts.length; i++)
         {
             com.intellij.openapi.actionSystem.Shortcut cut = cuts[i];
             if (cut instanceof KeyboardShortcut)
             {
+                // Remove the shortcut from the special plugin handler
                 keymap.removeShortcut("VimKeyHandler", cut);
                 KeyboardShortcut ks = (KeyboardShortcut)cut;
                 KeyStroke keyStroke = ks.getFirstKeyStroke();
+                // Get the list of IDEA actions that originally had this keystroke - if any
                 ArrayList actions = (ArrayList)mappings.get(keyStroke);
                 for (int j = 0; j < actions.size(); j++)
                 {
-                    keymap.addShortcut((String)actions.get(j), cut);
+                    // Put back the removed shortcut. But we need to "insert" it in the same place it was so the menus
+                    // will show the shortcuts. Example - Undo has by default two shortcuts - Ctrl-Z and Alt-Backspace.
+                    // When the plugin is disabled the Undo menu shows Ctrl-Z. When the plugin is enabled we remove
+                    // Ctrl-Z and the Undo menu shows Alt-Backspace. When the plugin is disabled again, we need to be
+                    // sure Ctrl-Z is put back before Alt-Backspace or else the Undo menu will continue to show
+                    // Alt-Backspace even after we add back Ctrl-Z.
+                    KeyAction ka = (KeyAction)actions.get(j);
+                    com.intellij.openapi.actionSystem.Shortcut[] acuts = keymap.getShortcuts(ka.getActionId());
+                    keymap.removeAllActionShortcuts(ka.getActionId());
+                    for (int k = 0, l = 0; k < acuts.length + 1; k++)
+                    {
+                        if (k == ka.getKeyPos())
+                        {
+                            keymap.addShortcut(ka.getActionId(), cut);
+                        }
+                        else
+                        {
+                            keymap.addShortcut(ka.getActionId(), acuts[l++]);
+                        }
+                    }
                 }
                 actions.clear();
             }
@@ -485,6 +522,28 @@ public class KeyParser
         res.append("]");
 
         return res.toString();
+    }
+
+    private static class KeyAction
+    {
+        public KeyAction(String actionId, int keyPos)
+        {
+            this.actionId = actionId;
+            this.keyPos = keyPos;
+        }
+
+        public String getActionId()
+        {
+            return actionId;
+        }
+
+        public int getKeyPos()
+        {
+            return keyPos;
+        }
+
+        private String actionId;
+        private int keyPos;
     }
 
     private HashMap keyRoots = new HashMap();
