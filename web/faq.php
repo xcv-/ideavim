df1f6dfceffd8df8e5f13ad990f0aff3e5eb6992
rmaddy <rmaddy>
2005-11-19 19:01:42 +0000

    Final doc changes for 0.10.4

--- web/faq.php
+++ web/faq.php
@@ -10,6 +10,9 @@
 <a href="#noplugin">
 The <code>Tools</code> menu doesn't have a new option called <code>VIM Emulator</code>
 </a><br>
+<a href="#nokeymap">
+There is no "Vim" keymap listed after selecting the <code>Options|Keymaps</code> menu
+</a><br>
 <a href="#pluginoff">
 Everything is installed properly but I just can't seem to enter VIM commands
 </a><br>
@@ -31,11 +34,21 @@ This most likely means you did not properly copy the plugin jar file into the pl
 sure you have copied the IdeaVIM.jar file from the installation download into the plugins directory.
 </dd>
 <dt>
+<a name="nokeymap"></a>
+There is no "Vim" keymap listed after selecting the <code>Options|Keymaps</code> menu
+</dt>
+<dd>
+Have you verified that you copied the Vim.xml file from the installation download into the
+&lt;HOME&gt;/.IntelliJIdea/config/keymaps directory?
+</dd>
+<dt>
 <a name="pluginoff"></a>
 Everything is installed properly but I just can't seem to enter VIM commands
 </dt>
 <dd>
-Select the <code>Tools</code> menu and make sure the menu <code>VIM Emulator</code> has a checkmark next to it.
+Select the <code>Tools</code> menu and make sure the menu <code>VIM Emulator</code> has a checkmark next to it. Also
+select the <code>Options|Keymaps</code> menu and make sure the "vIm" keymap says "active" next to it. If not, select
+the "Vim" keymap and press the <code>Set Active</code> button.
 </dd>
 <dt>
 <a name="turnoff"></a>
@@ -43,8 +56,8 @@ How do I turn off the VIM plugin once it is installed?
 </dt>
 <dd>
 If you wish to temporarily turn off the VIM plugin you must select the <code>Tools</code> menu an uncheck the
-<code>VIM Emulator</code> menu.
-Reverse this process to turn the VIM emulator back on. These settings will be saved
+<code>VIM Emulator</code> menu. You must also select the <code>Options|Keymaps</code> menu and make some other
+keymap active other than "Vim". Reverse this process to turn the VIM emulator back on. These settings will be saved
 and the next time you start IDEA, it will be in the same state as you left it.
 </dd>
 <!--
